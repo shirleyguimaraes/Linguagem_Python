528e4a4ebf1ff8c2e995b85253809740b8cf9a4a
Shirley <shirleybguimaraes07@gmail.com>
2021-10-29 19:54:05 -0300

    Inclusão do Arquivo
    O arquivo não foi incluído anteriormente.

---
+++ Curso_Coursera/EquacaoSegundoGrau.py
@@ -0,0 +1,44 @@
+import math
+
+'''Minha resolução'''
+'''
+def main():
+    a = int(input("Digite um valor para a: "))
+    b = int(input("Digite um valor para b: "))
+    c = int(input("Digite um valor para c: "))
+
+    equacao = ((b * b)-4 * (a * c))
+    raiz = int(math.sqrt(equacao))
+
+    delta1 = (-b + raiz) / (2 * a)
+    delta2 = (-b - raiz) / (2 * a)
+
+    if raiz == 0:
+        print("Tem uma raiz real:", delta1)
+    else:
+        print("Tem duas raiz real:", delta1, "e ", delta2)
+
+main()
+
+'''
+
+def main ():
+    a= float(input("Digite o valor de a: "))
+    b= float(input("Digite o valor de b: "))
+    c= float(input("Digite o valor de c: "))
+    
+    delta = b ** 2 - 4 * a * c
+
+    if delta == 0:
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        print ("A única raiz é: ", raiz1)
+    else:
+        if delta < 0:
+            print("Esta equação não possui raízes reais.")
+        else:
+            raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+            raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+            print("A primeira raiz é: ", raiz1)
+            print("A segunda raiz é: ", raiz2)
+
+main()
