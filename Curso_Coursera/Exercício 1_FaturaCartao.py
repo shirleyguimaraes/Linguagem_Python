c604604af405ae1dd526c1f5147134430f46e8d2
Shirley <shirleybguimaraes07@gmail.com>
2021-11-03 20:34:38 -0300

    Adicionar Arquivo
    Atividade do curso

---
+++ Curso_Coursera/Exercício 1_FaturaCartao.py
@@ -0,0 +1,7 @@
+cliente = input("Digite o nome do cliente: ")
+diaVencimento = int(input("Digite o dia de vencimento: "))
+mesVencimento = input("Digite o mês de vencimento: ")
+valorFatura= input("Digite o valor da fatura: ")
+
+print("Olá,", cliente)
+print("A sua fatura com vencimento em", diaVencimento, "de", mesVencimento, "no valor de R$", valorFatura, "está fechada.")
